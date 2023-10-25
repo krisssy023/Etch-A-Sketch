04ef830a1ed14165e7a03e723f644a8a1fa6adeb
kris <alaparkristian@gmail.com>
2023-10-25 23:34:54 +0800

    adding indexx, script and style files

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create a 16x16 grid using JavaScript
+const gridContainer = document.getElementById('grid-container');
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridContainer.appendChild(gridItem);
+    }
+}
